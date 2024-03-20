543042734f07bda81681afa6c7d4397cc7be2d45
Geos-coding-lab <145538041+mosongcodinglab@users.noreply.github.com>
2024-03-20 13:36:32 +0300

    Update settings.py

--- Deeployment/settings.py
+++ Deeployment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-##wf&bi9+y=!#^g^h(6%)z818i@(2$xuz)y96rs0o62t3lf=+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
